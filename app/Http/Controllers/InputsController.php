1142a1f64a4561fce312150f8e08f241b2e46db0
sweetroses009 <30721243+sweetroses009@users.noreply.github.com>
2021-08-18 14:48:33 +0500

    Initialize datatable

---
+++ app/Http/Controllers/InputsController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Payment;
+use Illuminate\Http\Request;
+use App\Models\Input;
+use DateTime;
+use Yajra\DataTables\Facades\DataTables;
+
+class InputsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+//        $datas = Input::latest()->with('payments')->get();
+
+//        foreach($datas as $data ){
+//            foreach($data->payments as $payment){
+//                echo $payment->name;
+//            }
+//        }
+
+
+        if ($request->ajax()) {
+            $data = Input::latest()->with('payments')->get();
+            return DataTables::of($data)
+                ->addIndexColumn()
+                ->addColumn('action', function($row){
+
+                    $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Edit" class="edit btn btn-primary btn-sm editProduct">Edit</a>';
+
+                    $btn = $btn.' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteProduct">Delete</a>';
+
+                    return $btn;
+                })
+                ->addColumn('payment', function ($data) {
+                    foreach($data->payments as $payments){
+                        return $payments->name;
+                    }
+                })
+                ->editColumn('date', function ($data){
+                    return date('d-m-yy', strtotime($data->date) );
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+
+        $payments = Payment::all();
+
+
+        return view('layout.dashboard',compact('payments'));
+//        return view('layout.dashboard',compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $myDateTime = DateTime::createFromFormat('d-m-Y', $request->get('date'));
+        $newDateString = $myDateTime->format('Y-m-d H:i');
+        $input = Input::updateOrCreate([
+                'user_id' => 1,
+                'description' => $request->description,
+                'import' => $request->import,
+                'date' => $newDateString
+            ]);
+
+
+        $input->payments()->attach($request->get('payment'),[
+            'paymentable_id' => $input->getAttribute('id'),
+            'paymentable_type' => 'App\Models\Input']);
+
+        return response()->json(['success'=>'Product saved successfully.']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/layout/template.blade.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Accounting System - @yield('title')</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+    <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
+
+    <!-- Gijgo Datepicker -->
+    <link href="{{ asset('gijgo/css/gijgo.min.css') }}" rel="stylesheet" type="text/css" />
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('css/simple-sidebar.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+
+<div class="d-flex" id="wrapper">
+
+    <!-- Sidebar -->
+    @include('layout.sidebar')
+    <!-- /#sidebar-wrapper -->
+
+    <div id="page-content-wrapper">
+
+        <!-- Navbar -->
+        @include('layout.navbar')
+        <!-- /#navbar-toggle -->
+
+        <!-- Content -->
+        <div class="container-fluid">
+            @yield('graphic')
+
+            @yield('content')
+        </div>
+        <!-- /#content -->
+
+    </div>
+    <!-- /#page-content-wrapper -->
+
+
+</div>
+
+<!-- Bootstrap core JavaScript -->
+<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+<script src="{{ asset('gijgo/js/gijgo.min.js') }}"></script>
+
+<!-- Custom JS for dashboard -->
+<script src="{{ asset('js/dashboard.js') }}"></script>
+{{--<script src="{{ asset('js/ajaxDataTAble.js') }}"></script>--}}
+
+@yield('scripts')
+
+</body>
+
+</html>
+
