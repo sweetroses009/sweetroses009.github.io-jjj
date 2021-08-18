c047acaf8edd506092050e6c2b135430e7123f3d
sweetroses009 <30721243+sweetroses009@users.noreply.github.com>
2021-08-18 14:48:33 +0500

    Initialize 2-Custom_Registration

---
+++ app/Models/Input.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Input extends Model
+{
+    protected $fillable = [
+        'user_id',  'description', 'import' ,'date'
+    ];
+
+    /**
+     * Get all of payments method.
+     */
+    public function payments()
+    {
+        return $this->morphToMany(Payment::class, 'paymentable');
+    }
+}
