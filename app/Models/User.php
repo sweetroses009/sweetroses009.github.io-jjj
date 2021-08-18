5fa2df973e5d2d16b5fa44468a6ffaaf6440c2b3
sweetroses009 <30721243+sweetroses009@users.noreply.github.com>
2021-08-18 14:48:35 +0500

    Initialize 5_Laravel_Charts

---
+++ app/Models/User.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'fstName','lstName','email', 'password','job_id'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * The roles that belong to the user.
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class,'role_user');
+    }
+
+    /**
+     * Get the job of a user.
+     */
+    public function job()
+    {
+        return $this->belongsTo(Job::class);
+    }
+
+    /**
+     * Get the output for user.
+     */
+    public function outputs()
+    {
+        return $this->hasMany(Output::class);
+    }
+
+    /**
+     * Get the output for user.
+     */
+    public function inputs()
+    {
+        return $this->hasMany(Input::class);
+    }
+}
